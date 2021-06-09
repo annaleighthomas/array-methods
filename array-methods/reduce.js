3c0d406e2c7e4edc12f11547d71b5895fc782d68
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-06-09 14:15:54 -0400

    reduce test passing and fun

--- array-methods/reduce.js
+++ array-methods/reduce.js
@@ -1,3 +1,7 @@
 export function reduce(arr, callback) {
-
+  let value = arr[0];
+  for(let i = 1; i < arr.length; i++) {
+    value = callback(value, arr[i]);
+  }
+  return value;
 }
--- array-methods/reduce.test.js
+++ array-methods/reduce.test.js
@@ -6,4 +6,10 @@ describe('the return value is passed as the accumulator argument of the next fun
     const output = reduce(input, (accumulator, item) => accumulator + item);
     expect(output).toEqual(10);
   });
+
+  it('should Accumulate all the value and return the sum of 24', () => {
+    const input = [1, 2, 3, 4];
+    const output = reduce(input, (accumulator, item) => accumulator * item);
+    expect(output).toEqual(24);
+  });
 });
