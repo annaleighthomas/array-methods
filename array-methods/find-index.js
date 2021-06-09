db904d8fee77d09d2fbc94e2d96224fa265a4efd
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-06-09 13:13:50 -0400

    passing find index test and func

--- array-methods/find-index.js
+++ array-methods/find-index.js
@@ -1,3 +1,8 @@
-export function findIndex(){
-
+export function findIndex(arr, callback){
+  for(let i = 0; i < arr.length; i++) {
+    if(callback(arr[i])){
+      return i;
+    } 
+  }
+  return -1;
 }
--- array-methods/find-index.test.js
+++ array-methods/find-index.test.js
@@ -1,9 +1,15 @@
 import { findIndex } from './find-index';
 
 describe('returns the index of the first item whose callback returns true or a truthy value', () => {
-  it('wil;l return the first number greater than 20', () => {
+  it('will return the first number greater than 20', () => {
     const input = [2, 6, 17, 24, 40, 54];
     const output = findIndex(input, item => item > 20);
-    expect(output).toEqual(24);
+    expect(output).toEqual(3);
+  });
+
+  it('will return -1 if not found', () => {
+    const input = [2, 6, 17, 24, 40, 54];
+    const output = findIndex(input, item => item > 60);
+    expect(output).toEqual(-1);
   });
 });
