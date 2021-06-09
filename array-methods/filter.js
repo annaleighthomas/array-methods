4b7c91b60608951acc9ee82b0e5fb6333fa094e7
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-06-08 23:24:29 -0400

    passing filter test

--- array-methods/filter.js
+++ array-methods/filter.js
@@ -1,3 +1,11 @@
 export function filter(arr, callback) {
-  
+  const finalArr = [];
+  let index = 0;
+  for(let i = 0; i < arr.length; i++) {
+    if(callback(arr[i])) {
+      finalArr[index] = arr[i];
+      index++;
+    }
+  }
+  return finalArr;
 }
